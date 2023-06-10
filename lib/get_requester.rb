db27b38b5ea1bd0fee6bef15871de4b91b87f6a0
sheringugi <sheri.ngugi17@gmail.com>
2023-06-10 13:26:02 +0300

    complete assignment

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,20 @@
-# Write your code here
+require 'net/http'
+require 'json'
+
+class GetRequester
+  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get(uri)
+    response.get_response_body
+  end
+
+  def parse_json
+    JSON.parse(self.get_response_body)
+  end
+end
